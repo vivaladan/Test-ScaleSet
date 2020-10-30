feca55ecc5a0300209dd9f17081b2c95646c1643
Daniel Revell <mail@danielrevell.com>
2020-10-30 11:23:52 +0000

    Update automate_nginx.sh

--- automate_nginx.sh
+++ automate_nginx.sh
@@ -2,4 +2,4 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx
-echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+echo "Hello World 1" | sudo tee -a /var/www/html/index.html
